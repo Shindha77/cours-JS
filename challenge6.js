611e1faa3d5d95a3a1f163517bc545d370071646
Jérémy Chalumeau <jchalumeau77@gmail.com>
2022-02-24 09:07:40 +0100

    challenge 6

---
+++ challenge6.js
@@ -0,0 +1,17 @@
+const calcAverage = (a, b, c) => {
+    const moyenne = (a + b +c) / 3;
+    return moyenne;
+}
+
+const dauphinsAverage = calcAverage(85, 54, 41);
+const koalasAverage = calcAverage(23, 34, 27);
+
+const checkWinner = function (average1, average2) {
+    if (average1 > average2) {
+        console.log(`Les Dauphins gagne ${average1} vs ${average2}`);
+    } else {
+        console.log(`Les Koalas gagne ${average2} vs ${average1}`);
+    }
+}
+
+checkWinner(dauphinsAverage, koalasAverage);
